a78e0ef42f17a29cd0ec0513a84493b465a0e696
Akromjon <akromjonrustamov56@gmail.com>
2024-05-26 17:20:18 +0500

    change config

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ SEND_GROUP_TEXT = lambda first_name, last_name, username, user_message: f"""
 FROM: 
 { first_name }
 { last_name }
-https://t.me{ username } \n
+https://t.me/{ username } \n
 
 MESSAGE: 
 { user_message }
